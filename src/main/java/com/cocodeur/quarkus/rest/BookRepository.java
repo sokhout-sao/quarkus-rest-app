0b6b7e01d914a608c0d3f6d6474da538333afc2d
Sok Hout SAO <sok-hout.sao@chanel.com>
2024-12-03 23:34:04 +0100

    Use configuration via application.properties

--- src/main/java/com/cocodeur/quarkus/rest/BookRepository.java
+++ src/main/java/com/cocodeur/quarkus/rest/BookRepository.java
@@ -2,20 +2,28 @@ package com.cocodeur.quarkus.rest;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
-    private List<Book> books = List.of(
-            new Book(1, "Clean Code", "Robert C. Martin", 2008, "IT"),
-            new Book(2, "The Pragmatic Programmer", "Andrew Hunt and David Thomas", 1999, "IT"),
-            new Book(3, "Design Patterns", "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides", 1994, "IT"),
-            new Book(4, "Code Complete", "Steve McConnell", 1993, "IT"),
-            new Book(5, "Refactoring", "Martin Fowler", 1999, "IT"),
-            new Book(6, "Refactoring", "Martin Fowler", 1999, "IT")
-    );
+
+    private final List<Book> books;
+
+    public BookRepository(
+            @ConfigProperty(name = "books.genre", defaultValue = "Sci-fi") String genre
+    ) {
+        this.books = List.of(
+                new Book(1, "Clean Code", "Robert C. Martin", 2008, genre),
+                new Book(2, "The Pragmatic Programmer", "Andrew Hunt and David Thomas", 1999, genre),
+                new Book(3, "Design Patterns", "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides", 1994, genre),
+                new Book(4, "Code Complete", "Steve McConnell", 1993, genre),
+                new Book(5, "Refactoring", "Martin Fowler", 1999, genre),
+                new Book(6, "Refactoring", "Martin Fowler", 1999, genre)
+        );
+    }
 
     public List<Book> getAllBooks() {
         return this.books;
